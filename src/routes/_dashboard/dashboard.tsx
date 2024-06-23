212d814e6e10cac3c5b058c5883d4575c46e5bf4
MbathaM <33201125+MbathaM@users.noreply.github.com>
2024-06-23 19:40:27 +0200

    Update dashboard.tsx

--- src/routes/_dashboard/dashboard.tsx
+++ src/routes/_dashboard/dashboard.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, Link, Outlet } from '@tanstack/react-router'
+import { createFileRoute, Outlet } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_dashboard/dashboard')({
   component: Dashboard
@@ -8,15 +8,6 @@ function Dashboard(){
   return (
     <>
 <div className="p-2">Hello from Dashboard!</div>
-<hr />
-    <div className="p-2 flex gap-2">
-  {/* <Link to="/signup" className="[&.active]:font-bold">
-    Signup
-  </Link> */}
-  <Link to="/dashboard/profile" className="[&.active]:font-bold">
-  profile
-  </Link>
-</div>
 <hr />
 <Outlet />
 </>
