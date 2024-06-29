f1941c6f36800c55ee3eedb42b31918d79f38dbe
MbathaM <33201125+MbathaM@users.noreply.github.com>
2024-06-29 21:22:33 +0200

    Create _dashboard.tsx

---
+++ src/routes/_dashboard.tsx
@@ -0,0 +1,27 @@
+import { Outlet, createFileRoute, redirect } from "@tanstack/react-router";
+
+import { SiteHeader } from "@/components/site-header";
+
+export const Route = createFileRoute("/_dashboard")({
+  beforeLoad: ({ context }) => {
+    const { isAuthenticated } = context.auth;
+
+    if (!isAuthenticated) {
+      throw redirect({ to: "/unauthenticated" });
+    }
+  },
+  component: DashboardLayout,
+});
+
+function DashboardLayout() {
+  return (
+    <div className="relative flex flex-col">
+      <SiteHeader />
+      <main className="container grow">
+        <section className="flex flex-col items-center justify-center gap-2 py-2 md:py-10">
+          <Outlet />
+        </section>
+      </main>
+    </div>
+  );
+}
